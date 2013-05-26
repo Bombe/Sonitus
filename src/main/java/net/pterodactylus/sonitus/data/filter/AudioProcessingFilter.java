bd7f4af5135a4b67164e12d31bd0904c673ab2cc
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2013-05-26 17:51:34 +0200

    Add internal audio processing filter.

---
+++ src/main/java/net/pterodactylus/sonitus/data/filter/AudioProcessingFilter.java
@@ -0,0 +1,63 @@
+/*
+ * Sonitus - AudioProcessingFilter.java - Copyright © 2013 David Roden
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.pterodactylus.sonitus.data.filter;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import net.pterodactylus.sonitus.data.Filter;
+import net.pterodactylus.sonitus.io.ProcessingOutputStream;
+
+/**
+ * {@link Filter} implementation that can process audio samples internally.
+ *
+ * @author <a href="mailto:bombe@pterodactylus.net">David ‘Bombe’ Roden</a>
+ */
+public abstract class AudioProcessingFilter extends DummyFilter {
+
+	//
+	// DUMMYFILTER METHODS
+	//
+
+	@Override
+	protected OutputStream createOutputStream() throws IOException {
+		OutputStream originalOutputStream = super.createOutputStream();
+		ProcessingOutputStream processingOutputStream = new ProcessingOutputStream(originalOutputStream, metadata().channels()) {
+
+			@Override
+			protected int[] processSamples(int[] samples) {
+				return AudioProcessingFilter.this.processSamples(samples);
+			}
+		};
+		return processingOutputStream;
+	}
+
+	//
+	// SUBCLASS METHODS
+	//
+
+	/**
+	 * Called to process the given of channels for a single sample.
+	 *
+	 * @param samples
+	 * 		The channel values of the sample
+	 * @return The processed channel values
+	 */
+	protected abstract int[] processSamples(int[] samples);
+
+}
